a0031ce6f9b62e4107346a88b210fd6144ebde5a
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-02-24 15:16:15 +0530

    through an error when is create duplicate doucment

--- mongoose/test.js
+++ mongoose/test.js
@@ -2,7 +2,7 @@ const { MongoServerSelectionError } = require('mongodb');
 const mongoose = require('mongoose');
 
 const connect = () => {
-    return mongoose.connect('mongodb://localhost:27017',)
+    return mongoose.connect('mongodb://localhost:27017/myDB',)
 }
 
 const student = new mongoose.Schema({
@@ -27,9 +27,10 @@ const Student = mongoose.model('student', student);
 connect()
     .then(async connection => {
         const student = await Student.create({ firstName: 'Aman' });
-        const found = await Student.find({ firstName: 'thi' });
-        const foundAll = await Student.find({}); // WILD CARD
-        const foundById = await Student.findById('idksdfjlijfilj');
-        const updated = await Student.findByIdAndUpdate('idksdfjlijfilj', {});
+        // const found = await Student.find({ firstName: 'thi' });
+        // const foundAll = await Student.find({}); // WILD CARD
+        // const foundById = await Student.findById('idksdfjlijfilj');
+        // const updated = await Student.findByIdAndUpdate('idksdfjlijfilj', {});
+        console.log(student)
     })
-    .catch(e => console.error(e))
+    .catch(e => console.error(e, 'Error'))
